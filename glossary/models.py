6d9de513262c4cad276620a81a1e3773b61c8648
suehtomit <58308039+suehtomit@users.noreply.github.com>
2020-01-20 13:46:08 +0100

    Defined the model.

--- glossary/models.py
+++ glossary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import validators
 
-# Create your models here.
+class GlossaryEntry(models.Model):
+    """Defines a model for an entry in the glossary that is displayed in the app."""
+    term    = models.CharField(max_length=100)
+    text    = models.TextField()
+    img     = models.ImageField(upload_to='glossary/images/', null=True, blank=True)
+    img_alt = models.CharField(max_length=200, blank=True, default='', validators=[validate_img_is_not_empty])
+    links   = models.ManyToManyField('self', symmetrical=False, blank=True)

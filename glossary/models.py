e2e3a517eb3e980691cc735e693c816b90f70c7b
chgad <chgad.games@gmail.com>
2020-02-11 10:29:58 +0100

    Removed validator in GlossaryEntry model.

--- glossary/models.py
+++ glossary/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-import validators
+
 
 class GlossaryEntry(models.Model):
     """Defines a model for an entry in the glossary that is displayed in the app."""
     term    = models.CharField(max_length=100)
     text    = models.TextField()
     img     = models.ImageField(upload_to='glossary/images/', null=True, blank=True)
-    img_alt = models.CharField(max_length=200, blank=True, default='', validators=[validate_img_is_not_empty])
+    img_alt = models.CharField(max_length=200, blank=True, default='')
     links   = models.ManyToManyField('self', symmetrical=False, blank=True)

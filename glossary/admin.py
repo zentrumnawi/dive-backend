0774c6717cd7708c0cb5f9b9a3430fc840a83618
chgad <chgad.games@gmail.com>
2020-02-11 11:44:59 +0100

    Implemented admin interface for GlosarryEntry.

--- glossary/admin.py
+++ glossary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import GlossaryEntry
 # Register your models here.
+
+
+class GlossaryEntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "term")
+
+
+admin.site.register(GlossaryEntry, GlossaryEntryAdmin)
--- glossary/models.py
+++ glossary/models.py
@@ -9,3 +9,6 @@ class GlossaryEntry(models.Model):
     img = models.ImageField(upload_to="glossary/images/", null=True, blank=True)
     img_alt = models.CharField(max_length=200, blank=True, default="")
     links = models.ManyToManyField("self", symmetrical=False, blank=True)
+    
+    def __str__(self):
+        return self.term

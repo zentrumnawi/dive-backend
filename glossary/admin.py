b4ec3df77a8438670eae31ceebafce83897551f7
chgad <chgad.games@gmail.com>
2020-02-11 11:57:50 +0100

    Implement validation for img_alt if img is given.

--- glossary/admin.py
+++ glossary/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from .models import GlossaryEntry
+from django import forms
 
 # Register your models here.
 
 
+class HasImgAltForm(forms.ModelForm):
+    def clean(self):
+        img = self.cleaned_data.get("img")
+        img_alt = self.cleaned_data.get("img_alt")
+        if img and not img_alt:
+            raise forms.ValidationError(
+                "An alternative image description needs to be provided."
+            )
+        super(HasImgAltForm, self).clean()
+
+
 class GlossaryEntryAdmin(admin.ModelAdmin):
+    form = HasImgAltForm
     list_display = ("id", "term")
 
 

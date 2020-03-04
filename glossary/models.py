8e27fdf237393da829639224fd58a0f63a4cd885
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-04 16:51:04 +0100

    Changed comment style and option upload_to in the img field

--- glossary/models.py
+++ glossary/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class GlossaryEntry(models.Model):
-    """Defines a model for an entry in the glossary that is displayed in the app."""
+    """
+    Defines a model for an entry in the glossary that is displayed in the app.
+    """
 
     term = models.CharField(max_length=100)
     text = models.TextField()
-    img = models.ImageField(upload_to="glossary/images/", null=True, blank=True)
+    img = models.ImageField(upload_to="glossary/", null=True, blank=True)
     img_alt = models.CharField(max_length=200, blank=True, default="")
     links = models.ManyToManyField("self", symmetrical=False, blank=True)
 

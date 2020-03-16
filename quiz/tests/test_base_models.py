c985ac18e6fbb0fe371edc8e7b909be1626822b6
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-16 18:13:36 +0100

    Implemented QuizQuestion model exists test

--- quiz/tests/test_base_models.py
+++ quiz/tests/test_base_models.py
@@ -1,1 +1,20 @@
 import pytest
+from django.db import models
+
+
+class TestQuizQuestionModelExists:
+    def test_model_exists(self):
+        """
+        This function tests whether an object QuizQuestion can be imported.
+        """
+        
+        from quiz.models import QuizQuestion
+        
+    def test_model_is_django_model(self):
+        """
+        This function tests if the QuizQuestion object is a Django model.
+        """
+        
+        from quiz.models import QuizQuestion
+        
+        assert issubclass(QuizQuestion, models.Model)

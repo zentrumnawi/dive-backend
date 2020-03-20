c303f9d3b1e8ef01e9b8e359e76ece075e3f8512
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-20 14:43:59 +0100

    Implemented validation for img_alt if img is given

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django import forms
 from .models import QuizQuestion
 
 
+class HasImgAltForm(forms.ModelForm):
+    def clean(self):
+        img = self.cleaned_data.get("img")
+        img_alt = self.cleaned_data.get("img_alt")
+        if img and not img_alt:
+            raise forms.ValidationError(
+                "An alternate text for the image needs to be provided."
+            )
+        super(HasImgAltForm, self).clean()
+
+
 class QuizQuestionAdmin(admin.ModelAdmin):
+    form = HasImgAltForm
     list_display = ("id", "text")
 
 

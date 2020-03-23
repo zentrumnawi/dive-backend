e277593b9addfd18c95fe7f9c84966fcd6d20936
christian <chgad.games@gmail.com>
2020-03-23 18:40:14 +0100

    Move Choices for Model fields to sepearte file for the sake of overview.

---
+++ content/choices.py
@@ -0,0 +1,236 @@
+NERV_CHOICES = (
+    ("str", _("streifennervig")),
+    ("net", _("netznervig")),
+    ("fie", _("fiedernervig")),
+    ("fin", _("fingernervig")),
+    ("fus", _("fußnervig")),
+    ("gab", _("gabelnervig"))
+)
+AXIS_CHOICES = (
+    ("ges", _("gestielt")),
+    ("sit", _("sitzend/ungestielt")),
+    ("ste", _("stengelumfassend")),
+    ("hal", _("halbstengelumfassend")),
+    ("dur", _("durchwachsen")),
+    ("ver", _("verwachsen")),
+    ("sch", _("scheidig verwachsen")),
+    ("her", _("herablaufend")),
+    ("rei", _("reitend")),
+    ("hin", _("hinfällig"))
+)
+POS_CHOICES = (
+    ("gru", _("grundständig")),
+    ("wec", _("wechselständig/spiralig")),
+    ("zwe", _("zweizeilig/distich")),
+    ("dre", _("dreizeilig/tristich")),
+    ("geg", _("gegenständig")),
+    ("kre", _("kreuzgegenständig/dekussiert")),
+    ("gep", _("gepaart")),
+    ("qui", _("quirlig/wirtelig")),
+    ("sch", _("scheinquirlig")),
+    ("dac", _("dachziegelig")),
+    ("ein", _("einseitig")),
+    ("ges", _("gescheitelt"))
+)
+SPR_WHOLE_CHOICES = (
+    ("ein", _("einfach")),
+    ("zus", _("zusammengesetzt"))
+)
+CUT_CHOICES = (
+    ("gan", _("ganz/ungeteilt")),
+    ("gel", _("gelappt")),
+    ("gep", _("gespalten")),
+    ("get", _("geteilt")),
+    ("ges", _("geschnitten"))
+)
+ARR_CHOICES = (
+    ("han", _("handförmig/fingerförmig")),
+    ("hgl", _("handförmig gelappt")),
+    ("hgt", _("handförmig geteilt")),
+    ("hgs", _("handförmig geschnitten")),
+    ("gef", _("gefingert")),
+    ("fie", _("fiederförmig/fiederig")),
+    ("fil", _("fiederlappig")),
+    ("fip", _("fiederspaltig")),
+    ("fit", _("fiederteilig")),
+    ("fis", _("fiederschnittig")),
+    ("paa", _("paarig gefiedert")),
+    ("unp", _("unpaarig gefiedert")),
+    ("unf", _("unterbrochen fiederschnittig")),
+    ("ung", _("unterbrochen gefiedert")),
+    ("dog", _("doppelt gefiedert")),
+    ("meh", _("mehrfach gefiedert")),
+    ("dre", _("dreizählig")),
+    ("dod", _("doppelt dreizählig")),
+    ("lei", _("leierförmig")),
+    ("sch", _("schrotsägeförmig")),
+    ("kam", _("kammförmig")),
+    ("fug", _("fußförmig geschnitten")),
+    ("fuz", _("fußförmig zusammengesetzt"))
+)
+FORM_CHOICES = (
+    ("kre", _("kreisrund")),
+    ("run", _("rundlich")),
+    ("ell", _("elliptisch")),
+    ("eif", _("eiförmig")),
+    ("ver", _("verkehrteiförmig")),
+    ("spa", _("spatelförmig/spatelig")),
+    ("len", _("lanzettlich")),
+    ("eil", _("eilanzettlich")),
+    ("lae", _("länglich")),
+    ("lin", _("linealisch/lineal")),
+    ("nad", _("nadelförmig")),
+    ("pfr", _("pfriemlich")),
+    ("bor", _("borstenförmig/borstlich")),
+    ("fad", _("fadenförmig/fädlich")),
+    ("bin", _("binsenartig")),
+    ("roe", _("röhrig")),
+    ("ser", _("schwertförmig")),
+    ("dre", _("dreieckig")),
+    ("kei", _("keilförmig")),
+    ("rau", _("rautenförmig/rhombisch")),
+    ("her", _("herzförmig")),
+    ("vhe", _("verkehrtherzförmig")),
+    ("nie", _("nierenförmig")),
+    ("pfe", _("pfeilförmig")),
+    ("spi", _("spießförmig")),
+    ("gei", _("geigenförmig")),
+    ("sup", _("schuppenförmig")),
+    ("sil", _("schildförmig"))
+)
+LEAFLET_CHOICES = (
+    ("gan", _("ganzrandig")),
+    ("ges", _("gesägt")),
+    ("dop", _("doppelt gesägt")),
+    ("rue", _("rückwärts gesägt")),
+    ("gez", _("gezähnt")),
+    ("gzt", _("gezähnelt")),
+    ("gef", _("gefranst")),
+    ("gek", _("gekerbt")),
+    ("geb", _("gebuchtet")),
+    ("ges", _("geschweift")),
+    ("bew", _("bewimpert")),
+    ("vor", _("vorwärts rauh")),
+    ("rra", _("rückwärts rauh"))
+)
+TEXTURE_CHOICES = (
+    ("kra", _("krautig")),
+    ("fle", _("fleischig")),
+    ("led", _("lederig")),
+    ("hae", _("häutig/trockenhäutig")),
+    ("imm", _("immergrün")),
+    ("sta", _("starr/steif"))
+)
+SUR_TEXTURE_CHOICES = (
+    ("gla", _("glatt")),
+    ("run", _("runzelig")),
+    ("rau", _("rauh")),
+    ("vra", _("vorwärts rauh")),
+    ("ber", _("bereift")),
+    ("bes", _("bestäubt/bemehlt")),
+    ("pap", _("papillös")),
+    ("pun", _("punktiert")),
+    ("swi", _("schwielig")),
+    ("kah", _("kahl")),
+    ("ver", _("verkahlend")),
+    ("fla", _("flaumhaarig/weichhaarig")),
+    ("sei", _("seidenhaarig")),
+    ("spi", _("spinnwebig")),
+    ("flo", _("flockig")),
+    ("sam", _("samthaarig")),
+    ("fil", _("filzig")),
+    ("wol", _("wollig")),
+    ("zot", _("zottig")),
+    ("rah", _("rauhaarig")),
+    ("ste", _("steifhaarig")),
+    ("gew", _("gewimpert")),
+    ("dru", _("drüsenhaarig")),
+    ("ste", _("sternhaarig")),
+    ("sdh", _("schildhaarig")),
+    ("sup", _("schuppenhaarig/schuppig")),
+    ("sue", _("schülferig")),
+    ("bae", _("bärtig")),
+    ("ach", _("achselbärtig")),
+    ("nac", _("nackt"))
+)
+SIDE_CHOICES = (
+    ("gan", _("ganzrandig")),
+    ("ges", _("gesägt")),
+    ("dop", _("doppelt gesägt")),
+    ("rue", _("rückwärts gesägt")),
+    ("gez", _("gezähnt")),
+    ("gzt", _("gezähnelt")),
+    ("gef", _("gefranst")),
+    ("gek", _("gekerbt")),
+    ("geb", _("gebuchtet")),
+    ("gew", _("geschweift")),
+    ("bew", _("bewimpert")),
+    ("vor", _("vorwärts rauh")),
+    ("rur", _("rückwärts rauh"))
+)
+DIAM_CHOICES = (
+    ("zur", _("zurückgerollt/umgerollt")),
+    ("ein", _("eingerollt")),
+    ("zus", _("zusammengerollt")),
+    ("gef", _("gefalzt/gefaltet")),
+    ("rin", _("rinnig")),
+    ("gek", _("gekielt")),
+    ("dop", _("doppelrillig"))
+)
+SP_CHOICES = (
+    ("kei", _("keilig/keilförmig")),
+    ("ver", _("verschmälert")),
+    ("abg", _("abgerundet")),
+    ("ges", _("gestutzt")),
+    ("her", _("herzförmig")),
+    ("nie", _("nierenförmig")),
+    ("pfe", _("pfeilförmig")),
+    ("spi", _("spießförmig"))
+)
+SP_TOP_CHOICES = (
+    ("abg", _("abgerundet")),
+    ("ges", _("gestzutzt")),
+    ("stu", _("stumpf")),
+    ("spi", _("spitz")),
+    ("zug", _("zugespitzt")),
+    ("beg", _("begrannt")),
+    ("sta", _("stachelspitze")),
+    ("haa", _("haarspitzig")),
+    ("bes", _("bespitzt")),
+    ("aus", _("ausgerandet")),
+)
+POSITION_CHOICES = (
+    ("auf", _("aufrecht")),
+    ("aua", _("aufrecht-abstehend")),
+    ("abs", _("abstehend")),
+    ("spa", _("sparrig")),
+    ("zur", _("zurückgeschlagen")),
+    ("lie", _("liegend")),
+    ("kri", _("kriechend")),
+    ("aus", _("aufsteigend/aufstrebend")),
+    ("gek", _("gekniet")),
+    ("ueb", _("übergebogen")),
+    ("nic", _("nickend")),
+    ("hae", _("hängend")),
+    ("flu", _("flutend")),
+    ("Ran", _("Rankenpflanze")),
+    ("Win", _("Windepflanze")),
+    ("Spr", _("Spreizklimmer")),
+    ("Wur", _("Wurzelkletterer")),
+    ("hor", _("horstig")),
+    ("loc", _("lockerrasig"))
+)
+SP_DIAM_CHOICES = (
+    ("sti", _("stielrund")),
+    ("hal", _("halbstielrund")),
+    ("zus", _("zusammengedrückt")),
+    ("zwe", _("zweischneidig")),
+    ("kan", _("kantig")),
+    ("stu", _("stumpfkantig")),
+    ("ger", _("gerieft /gerillt")),
+    ("gef", _("gefurcht")),
+    ("kan", _("kantig gefurcht /scharfkantig")),
+    ("gri", _("gerippt")),
+    ("gfl", _("geflügelt")),
+    ("kno", _("knotig"))
+)

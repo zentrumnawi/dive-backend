b28a5c9d7766bfece52f9f0a41ccaf194d135acf
christian <chgad.games@gmail.com>
2020-03-25 14:40:48 +0100

    Add Custom Serializers, and Env-variable to docker compose file.

--- content/serializers.py
+++ content/serializers.py
@@ -3,7 +3,48 @@ from importlib import import_module
 from django.conf import settings
 from rest_framework import serializers
 
-from .models import TreeNode
+from .models import TreeNode, Plant, Leaf, Sprout, Fruit, Blossom
+
+
+class LeafSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Leaf
+        exclude = ["plant"]
+
+
+class SproutSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Sprout
+        exclude = ["plant"]
+
+
+class FruitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Fruit
+        exclude = ["plant"]
+
+
+class BlossomSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Blossom
+        exclude = ["plant"]
+
+
+class PlantSerializer(serializers.ModelSerializer):
+
+    leaf = LeafSerializer()
+    sprout = SproutSerializer()
+    fruit = FruitSerializer()
+    blossom = BlossomSerializer()
+
+    class Meta:
+        model = Plant
+        fields = "__all__"
+        depth = 1
 
 
 class TreeNodeSerializer(serializers.ModelSerializer):

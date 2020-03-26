225a20e8a7533ef1444ef44a8a9a9f364f55c42e
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-26 20:21:38 +0100

    Implemented QuizQuestionEndpoint

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import QuizQuestion
+from .serializers import QuizQuestionSerializer
 
-# Create your views here.
+
+class QuizQuestionEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint provides the database table of all QuizQuestions.
+    """
+    
+    queryset = QuizQuestion.objects.all()
+    serializer_class = QuizQuestionSerializer
+    name = "quizquestion"

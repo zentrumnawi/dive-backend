d08822057a8e56865a9ed96c35e5ed9a733465a1
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-26 20:22:53 +0100

    Implemented QuizAnswerEndpoint

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from .models import QuizQuestion
-from .serializers import QuizQuestionSerializer
+from .models import QuizQuestion, QuizAnswer
+from .serializers import QuizQuestionSerializer, QuizAnswerSerializer
 
 
 class QuizQuestionEndpoint(ReadOnlyModelViewSet):
@@ -11,3 +11,13 @@ class QuizQuestionEndpoint(ReadOnlyModelViewSet):
     queryset = QuizQuestion.objects.all()
     serializer_class = QuizQuestionSerializer
     name = "quizquestion"
+
+
+class QuizAnswerEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint provides the database table of all QuizAnswers.
+    """
+    
+    queryset = QuizAnswer.objects.all()
+    serializer_class = QuizAnswerSerializer
+    name = "quizanswer"

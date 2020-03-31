b8e7b6dd1a1e0f27aea21b596238fff36a8699f0
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-31 19:20:22 +0200

    Modified QuizQuestionSerializer and QuizAnswerSerializer

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -2,13 +2,31 @@ from rest_framework import serializers
 from .models import QuizQuestion, QuizAnswer
 
 
-class QuizQuestionSerializer(serializers.ModelSerializer):
+class QuizQuestionLessSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
     
     class Meta:
         model = QuizQuestion
         fields = "__all__"
-        ordering = ("pk")
+        
+    def get_type(self, obj):
+        choice_dict = dict(obj.QTYPE_CHOICES)
+        return choice_dict.get(obj.type)
+
+
+class QuizAnswerLessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizAnswer
+        fields = ["id", "text", "correct", "feedback_correct", "feedback_incorrect"]
+
+
+class QuizQuestionSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+    answers = QuizAnswerLessSerializer(many=True)
+    
+    class Meta:
+        model = QuizQuestion
+        fields = ["id", "type", "difficulty", "text", "img", "img_alt", "tags", "answers"]
         
     def get_type(self, obj):
         choice_dict = dict(obj.QTYPE_CHOICES)
@@ -16,7 +34,7 @@ class QuizQuestionSerializer(serializers.ModelSerializer):
 
 
 class QuizAnswerSerializer(serializers.ModelSerializer):
-    question = QuizQuestionSerializer()
+    question = QuizQuestionLessSerializer()
     
     class Meta:
         model = QuizAnswer

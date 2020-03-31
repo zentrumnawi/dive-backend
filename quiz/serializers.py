724fc742caec77078a5430a4f3b89e1aa4607a4d
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-31 19:01:23 +0200

    Corrected errors in QuizQuestionSerializer and QuizAnswerSerializer

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -2,22 +2,21 @@ from rest_framework import serializers
 from .models import QuizQuestion, QuizAnswer
 
 
-class QuizQuestionSerializer(serializers.Modelserializer):
-    qtype = serializers.SerializerMethod()
-    answers = QuizAnswerSerializer(many=True)
+class QuizQuestionSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
     
     class Meta:
         model = QuizQuestion
         fields = "__all__"
-        ordering = ("pk",)
+        ordering = ("pk")
         
-    def get_qtype(self, obj):
+    def get_type(self, obj):
         choice_dict = dict(obj.QTYPE_CHOICES)
-        return choice_dict.get(obj.qtype)
+        return choice_dict.get(obj.type)
 
 
-class QuizAnswerSerializer(serializers.Modelserializer):
-    question = QuizQuestionSerializer
+class QuizAnswerSerializer(serializers.ModelSerializer):
+    question = QuizQuestionSerializer()
     
     class Meta:
         model = QuizAnswer

ef9674c7a89aa42e51500a2e2b007eb09792b124
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-03-31 19:29:26 +0200

    Removed HasImgAltForm

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,17 +3,6 @@ from django import forms
 from .models import QuizQuestion, QuizAnswer
 
 
-class HasImgAltForm(forms.ModelForm):
-    def clean(self):
-        img = self.cleaned_data.get("img")
-        img_alt = self.cleaned_data.get("img_alt")
-        if img and not img_alt:
-            raise forms.ValidationError(
-                "An alternate text for the image needs to be provided."
-            )
-        super(HasImgAltForm, self).clean()
-
-
 class QuizQuestionAdmin(admin.ModelAdmin):
     form = HasImgAltForm
     list_display = ("id", "text")

be20661ab2eb4b1ec15e5d264c0f7f33c9d74f6c
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-07 19:10:09 +0200

    Implemented Slideshow model

--- slideshow/models.py
+++ slideshow/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Slideshow(models.Model):
+    """
+    Model for a series of pages that can be switched back and forth.
+    """
+    
+    title = models.CharField(max_length=100, primary_key=True)
+    
+    def __str__(self):
+        return self.title

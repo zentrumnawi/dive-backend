af60b601800b4c29d8f3adbe62c683530caa3a7a
christian <chgad.games@gmail.com>
2020-04-07 16:19:59 +0200

    Add QuizAnswer to inlines in admin interface.

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,9 +3,14 @@ from .models import QuizQuestion, QuizAnswer
 from utility.forms import HasImgAltForm
 
 
+class QuizAnswerInline(admin.TabularInline):
+    model = QuizAnswer
+
+
 class QuizQuestionAdmin(admin.ModelAdmin):
     form = HasImgAltForm
     list_display = ("id", "text")
+    inlines = [QuizAnswerInline, ]
 
 
 admin.site.register(QuizQuestion, QuizQuestionAdmin)

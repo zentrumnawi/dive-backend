eae72e50893f2d5d5c1ccccacbb0d74641cfb558
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-07 19:08:34 +0200

    Implemented Slideshow model fields test

--- slideshow/tests/test_base_models.py
+++ slideshow/tests/test_base_models.py
@@ -14,3 +14,18 @@ class TestSlideshowModelExists:
         from slideshow.models import Slideshow
         
         assert issubclass(Slideshow, models.Model)
+
+
+class TestSlideshowModelFields:
+    """
+    Test suite with basic field tests whether all fields of the Slideshow
+    object exist and have the correct class instance.
+    """
+    
+    def test_model_has_field_titel(self, slideshow_model_class):
+        assert hasattr(slideshow_model_class, "title")
+        
+    def test_field_type_title(self, slideshow_model_class):
+        assert isinstance(
+            slideshow_model_class._meta.get_field("title"), models.CharField
+        )

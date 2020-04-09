c9cbe4209991e98032ed3eda93bc746e33b5a2c2
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 21:54:55 +0200

    Implemented admin interface for the SlideshowImage model

--- slideshow/admin.py
+++ slideshow/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Slideshow, SlideshowPage
+from .models import Slideshow, SlideshowPage, SlideshowImage
+from utility.forms import HasImgAltForm
 
 
 admin.site.register(Slideshow, admin.ModelAdmin)
 admin.site.register(SlideshowPage, admin.ModelAdmin)
+
+
+class SlideshowImageAdmin(admin.ModelAdmin):
+    form = HasImgAltForm
+    list_display = ["id", "page", "position", "title", "img", "img_alt"]
+
+admin.site.register(SlideshowImage, SlideshowImageAdmin)

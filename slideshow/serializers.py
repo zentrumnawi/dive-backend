c385f384d4c50045b8ea97aa76ea47c5daaa8192
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 17:48:18 +0200

    Implemented SlideshowPageSerializer and modified SlideshowSerializer

--- slideshow/serializers.py
+++ slideshow/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Slideshow
+from .models import Slideshow, SlideshowPage
+
+
+class SlideshowPageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SlideshowPage
+        fields = ["id", "position", "title", "text"]
 
 
 class SlideshowSerializer(serializers.ModelSerializer):
+    pages = SlideshowPageSerializer(many=True)
+
     class Meta:
         model = Slideshow
         fields = "__all__"

83b35fe357544412f5806a76f44d93d162f89d85
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 16:04:05 +0200

    Implemented SlideshowPage fixture

--- slideshow/tests/conftest_files/general_conftest.py
+++ slideshow/tests/conftest_files/general_conftest.py
@@ -1,7 +1,13 @@
 import pytest
-from slideshow.models import Slideshow
+from slideshow.models import Slideshow, SlideshowPage
 
 
 @pytest.fixture
 def slideshow_model_class():
     return Slideshow
+
+
+@pytest.fixture
+def slideshow_page_model_class():
+    return SlideshowPage
+

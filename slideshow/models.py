de01c1a6b6def4e6ccc0b3eb6d3af3766ffb6be2
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 16:10:19 +0200

    Implemented SlideshowPage model

--- slideshow/models.py
+++ slideshow/models.py
@@ -10,3 +10,25 @@ class Slideshow(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class SlideshowPage(models.Model):
+    """
+    Model for a page of the Slideshow model.
+    """
+    
+    show = models.ForeignKey(
+        to=Slideshow,
+        on_delete=models.PROTECT,
+        related_name="pages",
+        related_query_name="page",
+        db_index=False
+    )
+    position = models.PositiveSmallIntegerField(
+        default=1, #validators=[validate_position_occupied]
+    )
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    
+    def __str__(self):
+        return self.title

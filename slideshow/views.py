42a0735917fb3e56fb0701a023c9576fc49f7522
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 22:15:07 +0200

    Changed comment in SlideshowEndpoint

--- slideshow/views.py
+++ slideshow/views.py
@@ -5,7 +5,8 @@ from .serializers import SlideshowSerializer
 
 class SlideshowEndpoint(ReadOnlyModelViewSet):
     """
-    Endpoint that provides the database table of all Slideshows.
+    Endpoint that provides the database table of all Slideshows including their
+    pages and images.
     """
     
     queryset = Slideshow.objects.all()

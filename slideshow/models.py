b23395fc38a79bf87b6a2506db54aa9293e413da
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 21:18:26 +0200

    Implemented SlideshowImage model

--- slideshow/models.py
+++ slideshow/models.py
@@ -32,3 +32,27 @@ class SlideshowPage(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class SlideshowImage(models.Model):
+    """
+    Model for an image for a page of the Slideshow model.
+    """
+    
+    page = models.ForeignKey(
+        to=SlideshowPage,
+        on_delete=models.CASCADE,
+        related_name="images",
+        related_query_name="image",
+        db_index=False
+    )
+    position = models.PositiveSmallIntegerField(
+        default=1, #validators=[validate_position_occupied]
+    )
+    title = models.CharField(max_length=100)
+    img = models.ImageField(upload_to="slideshow/")
+    img_alt = models.CharField(max_length=200)
+    caption = models.TextField(default="", blank=True)
+    
+    def __str__(self):
+        return self.img_alt

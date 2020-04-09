45ec466488dd5dce7ea495640cb5b96a16dbad50
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 18:22:04 +0200

    Implemented SlideshowImage model fields test

--- slideshow/tests/test_base_models.py
+++ slideshow/tests/test_base_models.py
@@ -96,3 +96,58 @@ class TestSlideshowPageModelFields:
         assert isinstance(
             slideshow_page_model_class._meta.get_field("text"), models.TextField
         )
+
+
+class TestSlideshowImageModelFields:
+    """
+    Test suite with basic field tests whether all fields of the SlideshowImage
+    object exist and have the correct class instance.
+    """
+    
+    def test_model_has_field_page(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "page")
+        
+    def test_model_has_field_position(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "position")
+        
+    def test_model_has_field_title(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "title")
+        
+    def test_model_has_field_img(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "img")
+        
+    def test_model_has_field_img_alt(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "img_alt")
+        
+    def test_model_has_field_caption(self, slideshow_image_model_class):
+        assert hasattr(slideshow_image_model_class, "caption")
+    
+    def test_field_type_page(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("page"), models.ForeignKey
+        )
+        
+    def test_field_type_position(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("position"), models.PositiveSmallIntegerField
+        )
+        
+    def test_field_type_title(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("title"), models.CharField
+        )
+        
+    def test_field_type_img(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("img"), models.ImageField
+        )
+        
+    def test_field_type_img_alt(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("img_alt"), models.CharField
+        )
+        
+    def test_field_type_caption(self, slideshow_image_model_class):
+        assert isinstance(
+            slideshow_image_model_class._meta.get_field("caption"), models.TextField
+        )

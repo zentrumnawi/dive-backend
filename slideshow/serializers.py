d0286587ba338022613018857fe446319e485e0b
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-09 21:55:57 +0200

    Implemented SlideshowImageSerializer and modified SlideshowPageSerializer

--- slideshow/serializers.py
+++ slideshow/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from .models import Slideshow, SlideshowPage
+from .models import Slideshow, SlideshowPage, SlideshowImage
+
+
+class SlideshowImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SlideshowImage
+        fields = ["id", "position", "title", "img", "img_alt", "caption"]
 
 
 class SlideshowPageSerializer(serializers.ModelSerializer):
+    images = SlideshowImageSerializer(many=True)
+    
     class Meta:
         model = SlideshowPage
-        fields = ["id", "position", "title", "text"]
+        fields = ["id", "position", "title", "text", "images"]
 
 
 class SlideshowSerializer(serializers.ModelSerializer):

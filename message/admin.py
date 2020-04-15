1dd18401d2f097118bf9c231d7d9f15a6cd84351
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-15 20:10:11 +0200

    Implemented admin interface for the Message model

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message
+from utility.forms import HasImgForm, DateOrderForm
 
-# Register your models here.
+
+class MessageForm(HasImgForm, DateOrderForm):
+    pass
+
+
+class MessageAdmin(admin.ModelAdmin):
+    form = MessageForm
+    list_display = ["id", "type", "title", "valid_from", "valid_to", "img"]
+
+admin.site.register(Message, MessageAdmin)

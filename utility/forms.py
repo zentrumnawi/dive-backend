d3bb5a96c288aeef4fcaaada7e61310f3c8e7047
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-15 20:02:26 +0200

    Renamed HasImgAltForm to HasImgForm and implemented additional validation of img_alt field

--- utility/forms.py
+++ utility/forms.py
@@ -1,12 +1,24 @@
 from django import forms
 
 
-class HasImgAltForm(forms.ModelForm):
+class HasImgForm(forms.ModelForm):
+    """
+    Validation if an image and an alternate text for the image are provided
+    together.
+    """
+    
     def clean(self):
         img = self.cleaned_data.get("img")
         img_alt = self.cleaned_data.get("img_alt")
+        
         if img and not img_alt:
             raise forms.ValidationError(
                 "An alternate text for the image needs to be provided."
             )
-        super(HasImgAltForm, self).clean()
+        
+        if img_alt and not img:
+            raise forms.ValidationError(
+                "Please select an image for upload or leave the \"img alt\" field empty."
+        )
+        
+        super(HasImgForm, self).clean()

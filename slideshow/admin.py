12e1a5bc45ed97b49f1eeae3373c63227d972b55
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-17 09:35:24 +0200

    Reduced display lists in admin interfaces

--- slideshow/admin.py
+++ slideshow/admin.py
@@ -7,13 +7,13 @@ admin.site.register(Slideshow, admin.ModelAdmin)
 
 
 class SlideshowPageAdmin(admin.ModelAdmin):
-    list_display = ["id", "show", "position", "title", "text"]
+    list_display = ["id", "show", "position", "title"]
 
 admin.site.register(SlideshowPage, SlideshowPageAdmin)
 
 
 class SlideshowImageAdmin(admin.ModelAdmin):
     form = HasImgForm
-    list_display = ["id", "page", "position", "title", "img", "img_alt"]
+    list_display = ["id", "page", "position", "title", "img"]
 
 admin.site.register(SlideshowImage, SlideshowImageAdmin)

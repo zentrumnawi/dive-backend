db38751f35a5c075772bbd0870725be51388cff6
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-04-17 10:50:16 +0200

    Implemented router for SlideshowImageEndpoint

--- slideshow/urls.py
+++ slideshow/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import SimpleRouter
-from .views import SlideshowEndpoint, SlideshowPageEndpoint
+from .views import SlideshowEndpoint, SlideshowPageEndpoint, SlideshowImageEndpoint
 
 
 app_name = "slideshow"
 router = SimpleRouter()
 router.register(r"slideshow", SlideshowEndpoint)
 router.register(r"slideshowpage", SlideshowPageEndpoint)
+router.register(r"slideshowimage", SlideshowImageEndpoint)
 urlpatterns = []
 urlpatterns += router.urls

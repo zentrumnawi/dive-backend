afb83832e8ceccd8f86a5f44919753b760827b41
christian <chgad.games@gmail.com>
2020-04-24 11:53:54 +0200

    Remove SerializerMethod field of Question Serializers, no longer needed.

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -3,15 +3,9 @@ from .models import QuizQuestion, QuizAnswer
 
 
 class QuizQuestionLessSerializer(serializers.ModelSerializer):
-    type = serializers.SerializerMethodField()
-
     class Meta:
         model = QuizQuestion
         fields = "__all__"
-
-    def get_type(self, obj):
-        choice_dict = dict(obj.QTYPE_CHOICES)
-        return choice_dict.get(obj.type)
 
 
 class QuizAnswerLessSerializer(serializers.ModelSerializer):
@@ -21,16 +15,11 @@ class QuizAnswerLessSerializer(serializers.ModelSerializer):
 
 
 class QuizQuestionSerializer(serializers.ModelSerializer):
-    type = serializers.SerializerMethodField()
     answers = QuizAnswerLessSerializer(many=True)
 
     class Meta:
         model = QuizQuestion
         fields = "__all__"
-
-    def get_type(self, obj):
-        choice_dict = dict(obj.QTYPE_CHOICES)
-        return choice_dict.get(obj.type)
 
 
 class QuizAnswerSerializer(serializers.ModelSerializer):

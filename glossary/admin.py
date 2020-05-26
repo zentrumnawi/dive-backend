7e1bda47aa75af72014da7107bf63357f0ce21b3
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-05-26 16:11:25 +0200

    Adapted GlossaryEntryAdmin

--- glossary/admin.py
+++ glossary/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import GlossaryEntry
-from utility.forms import HasImgForm
 
 
 class GlossaryEntryAdmin(admin.ModelAdmin):
-    form = HasImgForm
-    list_display = ("id", "term")
+    list_display = ["id", "term"]
 
 
 admin.site.register(GlossaryEntry, GlossaryEntryAdmin)

a3ce3f1f82e19bd7281e63fd85911916a97c37fb
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-05-26 17:05:05 +0200

    Formatted code with black

--- glossary/migrations/0004_remove_img_fields.py
+++ glossary/migrations/0004_remove_img_fields.py
@@ -6,16 +6,10 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('glossary', '0003_add_verbose_name_to_text_field'),
+        ("glossary", "0003_add_verbose_name_to_text_field"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='glossaryentry',
-            name='img',
-        ),
-        migrations.RemoveField(
-            model_name='glossaryentry',
-            name='img_alt',
-        ),
+        migrations.RemoveField(model_name="glossaryentry", name="img",),
+        migrations.RemoveField(model_name="glossaryentry", name="img_alt",),
     ]

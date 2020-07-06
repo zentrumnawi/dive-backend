2e036765830da884b5f0ee6f00f40135ebd3b930
christian <chgad.games@gmail.com>
2020-07-06 15:02:12 +0200

    Switch project setup from pipenv to poetry.

--- docker/local/entrypoint.sh
+++ docker/local/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-while ! pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT; do
+while ! /wait-for-it.sh -h $POSTGRES_HOST -p $POSTGRES_PORT; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
@@ -9,3 +9,4 @@ done
 >&2 echo "Postgres is up - continuing"
 
 exec "$@"
+

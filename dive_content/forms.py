d4f96ecd6e29e445eb7c1b8c3c3d610fe9a19ac1
christian <chgad.games@gmail.com>
2020-07-10 11:52:39 +0200

    Fix not working multiple Choice selection in Plant model.

--- dive_content/forms.py
+++ dive_content/forms.py
@@ -1,49 +1,12 @@
 from django import forms
-from django.contrib.postgres.fields import ArrayField
-from django.utils.datastructures import MultiValueDict
 
 from .models import Plant
 
-
-class ArrayFieldSelectMultiple(forms.SelectMultiple):
-    """This is a Form Widget for use with a Postgres ArrayField. It implements
-    a multi-select interface that can be given a set of `choices`.
-
-    You can provide a `delimiter` keyword argument to specify the delimeter used.
-
-    """
-
-    def __init__(self, *args, **kwargs):
-        # Accept a `delimiter` argument, and grab it (defaulting to a comma)
-        self.delimiter = kwargs.pop("delimiter", ",")
-        super(ArrayFieldSelectMultiple, self).__init__(*args, **kwargs)
-
-    def render_options(self, choices, value):
-        # value *should* be a list, but it might be a delimited string.
-        if isinstance(
-            value, str
-        ):  # python 2 users may need to use basestring instead of str
-            value = value.split(self.delimiter)
-        return super(ArrayFieldSelectMultiple, self).render_options(choices, value)
-
-    def value_from_datadict(self, data, files, name):
-        if isinstance(data, MultiValueDict):
-            # Normally, we'd want a list here, which is what we get from the
-            # SelectMultiple superclass, but the SimpleArrayField expects to
-            # get a delimited string, so we're doing a little extra work.
-            return self.delimiter.join(data.getlist(name))
-        return data.get(name, None)
-
-
 class PlantModelForm(forms.ModelForm):
+
+    habitat = forms.MultipleChoiceField(choices=Plant.HABITAT_CHOICES)
+    ground = forms.MultipleChoiceField(choices=Plant.GROUND_CHOICES)
+
     class Meta:
         model = Plant
-        widgets = {
-            "habitat": ArrayFieldSelectMultiple(
-                choices=Plant.HABITAT_CHOICES, attrs={"class": "chosen"}
-            ),
-            "ground": ArrayFieldSelectMultiple(
-                choices=Plant.GROUND_CHOICES, attrs={"class": "chosen"}
-            ),
-        }
         fields = "__all__"

84e4f9c717be509835252c538957734f6c219357
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-03-01 13:38:42 +0100

    Alter PlantAdminForm.habitat and .ground

--- dive_content/forms.py
+++ dive_content/forms.py
@@ -4,8 +4,16 @@ from .models import Plant
 
 
 class PlantAdminForm(forms.ModelForm):
-    habitat = forms.MultipleChoiceField(choices=Plant.HABITAT_CHOICES)
-    ground = forms.MultipleChoiceField(choices=Plant.GROUND_CHOICES)
+    habitat = forms.MultipleChoiceField(
+        choices=Plant.HABITAT_CHOICES,
+        required=False,
+        label=Plant._meta.get_field("habitat").verbose_name,
+    )
+    ground = forms.MultipleChoiceField(
+        choices=Plant.GROUND_CHOICES,
+        required=False,
+        label=Plant._meta.get_field("ground").verbose_name,
+    )
 
     class Meta:
         model = Plant

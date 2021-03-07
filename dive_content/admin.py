943b6521f8895f35d22478f92a8afa9b6fc1d601
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-03-07 17:08:52 +0100

    Alter LeafAdmin. and LeafInline.readonly_fields

--- dive_content/admin.py
+++ dive_content/admin.py
@@ -9,7 +9,7 @@ from .models import Blossom, Fruit, Leaf, Plant, Sprout, ZeigerNumber
 class LeafInline(admin.StackedInline):
     model = Leaf
     form = LeafAdminForm
-    readonly_fields = ("get_att_axis_output",)
+    readonly_fields = ("get_att_axis_output", "get_dep_cuts_output")
     classes = ("collapse",)
 
 
@@ -54,7 +54,7 @@ admin.site.register(Plant, PlantAdmin)
 class LeafAdmin(admin.ModelAdmin):
     model = Leaf
     form = LeafAdminForm
-    readonly_fields = ("get_att_axis_output",)
+    readonly_fields = ("get_att_axis_output", "get_dep_cuts_output")
 
 
 admin.site.register(Leaf, LeafAdmin)

3ca92ce300bef2cd65aca3bf2bcfd8a478f0d429
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-03-10 11:14:07 +0100

    Alter LeafAdmin. and LeafInline.readonly_fields

--- dive_content/admin.py
+++ dive_content/admin.py
@@ -16,6 +16,7 @@ class LeafInline(admin.StackedInline):
         "get_blade_undiv_output",
         "get_margin_output",
         "get_surface_output",
+        "get_stipule_margin_output",
     )
     classes = ("collapse",)
 
@@ -67,6 +68,7 @@ class LeafAdmin(admin.ModelAdmin):
         "get_blade_div_output",
         "get_blade_undiv_output",
         "get_surface_output",
+        "get_stipule_margin_output",
     )
 
 

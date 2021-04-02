a09f4f69392b854ef7a440301b4104cdbb6fb8d9
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-04-02 14:51:57 +0200

    Create IndicatorsAdminForm

--- dive_content/forms.py
+++ dive_content/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from .choices import *
-from .fields import ArrayMultipleChoiceField, IntegerRangeCharField
+from .fields import ArrayMultipleChoiceField, IntegerRangeCharField, IndicatorField
 from .models import Fruit, Leaf, Plant, StemRoot
 
 
@@ -66,3 +66,37 @@ class StemRootAdminForm(forms.ModelForm):
         StemRoot, "cross_section", choices=SR_CROSS_SECTION_CHOICES
     )
     surface = ArrayMultipleChoiceField(StemRoot, "surface", choices=SURFACE_CHOICES)
+
+
+class IndicatorsAdminForm(forms.ModelForm):
+    light = IndicatorField("light")
+    temperature = IndicatorField("temperature")
+    humidity = IndicatorField("humidity")
+    reaction = IndicatorField("reaction")
+    nitrogen = IndicatorField("nitrogen")
+
+    def save(self, commit=True):
+        # Populate key field.
+        indicators_values = [self.cleaned_data.get(i, "") for i in INDICATORS]
+        symbols = [
+            "()" in indicators_values[0],
+            any("(?)" in value for value in indicators_values[:2]),  # Ensure key order.
+            "~" in indicators_values[2],
+            "=" in indicators_values[2],
+            any("(?)" in value for value in indicators_values[2:]),  # Ensure key order.
+        ]
+
+        instance = super().save(commit=False)
+        instance.key = []
+        for i, symbol in enumerate(symbols):
+            if symbol:
+                if i == 4:
+                    if not symbols[1]:
+                        instance.key.append(KEY_CHOICES[1][0])
+                else:
+                    instance.key.append(KEY_CHOICES[i][0])
+
+        if commit:
+            instance.save()
+
+        return instance

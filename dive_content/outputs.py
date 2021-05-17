bcdbcb4c07c21e7b63d153c79ca33bf38110bdf1
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-05-17 12:33:30 +0200

    Create add_suffix()

--- dive_content/outputs.py
+++ dive_content/outputs.py
@@ -1,1 +1,14 @@
 from .choices import *
+
+
+def add_suffix(term, suffix, separator=None):
+    if term:
+        if separator:
+            splited_term = term.split(separator)
+            for i, st in enumerate(splited_term):
+                splited_term[i] = f"{st}{suffix}"
+            term = separator.join(splited_term)
+        else:
+            term = f"{term}{suffix}"
+
+    return term

442fe820e55343bc7d0da42c0c7e5e2235245223
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-08-13 16:33:21 +0200

    Create TrivialNamesWidget

--- dive_content/widgets.py
+++ dive_content/widgets.py
@@ -170,3 +170,30 @@ class TrivialNameWidget(forms.MultiWidget):
 
     def decompress(self, value):
         return value.split(",") if value else []
+
+
+class TrivialNamesWidget(forms.MultiWidget):
+    template_name = "multiwidget.html"
+
+    def __init__(self, attrs=None):
+        widgets = [
+            forms.TextInput(
+                attrs={
+                    "size": 25,
+                    "maxlength": 50,
+                    "placeholder": "Trivialname",
+                    "required": False,
+                }
+            )
+        ] * 4
+
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        return value.split(",") if value else []
+
+    def value_from_datadict(self, data, files, name):
+        value = super().value_from_datadict(data, files, name)
+        value = list(filter(None, value))
+
+        return value
